e7fb05723e8c8a770fc7a95464280d4d81303742
Oleh Zymenko <oleg.zymenko.13@gmail.com>
2025-08-04 14:30:30 +0300

    Add location

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Geolocator.requestPermission();
   runApp(const MapLinkApp());
 }
 
@@ -11,7 +14,11 @@ class MapLinkApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyHomePage());
+    return MaterialApp(
+      title: 'Map Link',
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+    );
   }
 }
 
@@ -22,32 +29,70 @@ class MyHomePage extends StatefulWidget {
 }
 
 class MyHomePageState extends State<MyHomePage> {
+  late final MapController _mapController = MapController();
+  LatLng? _currentPosition;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('Map link', style: TextStyle(fontSize: 22)),
+        title: const Text('Map link', style: TextStyle(fontSize: 22)),
       ),
-      body: content(),
+      body: Stack(
+        children: [
+          FlutterMap(
+            mapController: _mapController,
+            options: MapOptions(
+              initialCenter: LatLng(1.2878, 103.8666),
+              initialZoom: 11,
+              interactionOptions: const InteractionOptions(
+                flags: ~InteractiveFlag.doubleTapZoom,
+              ),
+            ),
+            children: [
+              TileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+              ),
+              if (_currentPosition != null)
+                MarkerLayer(
+                  markers: [
+                    Marker(
+                      width: 50,
+                      height: 50,
+                      point: _currentPosition!,
+                      child: const Icon(
+                        Icons.my_location,
+                        color: Colors.blue,
+                        size: 24,
+                      ),
+                    ),
+                  ],
+                ),
+            ],
+          ),
+
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: FloatingActionButton(
+              backgroundColor: Colors.white,
+              onPressed: _goToMyLocation,
+              child: const Icon(Icons.my_location, color: Colors.blue),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
-  Widget content() {
-    return FlutterMap(
-      options: MapOptions(
-        initialCenter: LatLng(47.90620898132502, 33.47209458650657),
-        initialZoom: 11,
-        interactionOptions: const InteractionOptions(
-          flags: ~InteractiveFlag.doubleTapZoom,
-        ),
-      ),
-      children: [openStreetMapTileLayer],
-    );
+  Future<void> _goToMyLocation() async {
+    final position = await Geolocator.getCurrentPosition();
+    final latLng = LatLng(position.latitude, position.longitude);
+    setState(() {
+      _currentPosition = latLng;
+    });
+    _mapController.move(latLng, _mapController.camera.zoom);
   }
 }
-
-TileLayer get openStreetMapTileLayer => TileLayer(
-  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-  userAgentPackageName: 'dev.fleaflet.flutter_map.example',
-);
